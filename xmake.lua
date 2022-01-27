d89b4d0c5d2dcc277897f185cb74b7c0e5c72cea
charles seizilles <charles.seizilles@iconeus.com>
2022-01-27 17:34:53 +0100

    Add xmake task for CI

--- xmake.lua
+++ xmake.lua
@@ -3,6 +3,31 @@ add_requires("catch2")
 
 set_arch("x64")
 
+-- Generates a hash key made of packages confs/version, for CI
+task("dephash")
+	on_run(function ()
+		import("core.project.project")
+		import("private.action.require.impl.package")
+
+		local requires, requires_extra = project.requires_str()
+
+		local key = {}
+		for _, instance in irpairs(package.load_packages(requires, {requires_extra = requires_extra})) do
+			table.insert(key, instance:name() .. "-" .. instance:version_str() .. "-" .. instance:buildhash())
+		end
+
+		table.sort(key)
+
+		key = table.concat(key, ",")
+		print(hash.uuid4(key):gsub('-', ''):lower())
+	end)
+
+	set_menu {
+		usage = "xmake dephash",
+		description = "Outputs a hash key of current dependencies version/configuration"
+	}
+task_end()
+
 target("testCdm")
 	set_kind("binary")
 	set_languages("c++20")
